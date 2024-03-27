e2515300fa6663894efa625bdff5eafc53a014b8
Khotso Tsoaela <khotso.s.tsoaela@gmail.com>
2024-03-27 20:43:13 +0200

    setting up docker-compose

--- LIPAMPIRI/LIPAMPIRI/settings.py
+++ LIPAMPIRI/LIPAMPIRI/settings.py
@@ -102,6 +102,24 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.getenv('ENGINE'),
+#         "NAME": os.getenv('NAME'),
+#         "USER": os.getenv('USER'),
+#         "PASSWORD": os.getenv('PASSWORD'),
+#         "HOST": os.getenv('HOST'),
+#         "PORT": os.getenv('PORT'),
+#     }
+# }
+
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND'),
+EMAIL_HOST = os.getenv('EMAIL_HOST'),
+EMAIL_PORT = os.getenv('EMAIL_PORT'),
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER'),
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD'),
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS'),
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
